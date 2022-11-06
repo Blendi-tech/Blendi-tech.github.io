8523c56757cef3321510854e0708d6280fe43dac
Blendi-tech <114534377+Blendi-tech@users.noreply.github.com>
2022-11-06 19:38:34 +0100

    Uploading

---
+++ blendi.js
@@ -0,0 +1,128 @@
+
+function log() {
+	let tlogo = document.getElementById('t');
+	let flogo = document.getElementById('l1');
+	let img = "phone1.png";
+	tlogo.style.color = "white";
+	flogo.src = img;
+}
+function logout() {
+	let tlogo = document.getElementById('t');
+	let flogo = document.getElementById('l1');
+	let img = "phone.png";
+	tlogo.style.color = "black";
+	flogo.src = img;
+}
+
+
+function log1() {
+	let tlogo = document.getElementById('t1');
+	let flogo = document.getElementById('l2');
+	let img = "email1.png";
+	tlogo.style.color = "white";
+	flogo.src = img;
+}
+function log1out() {
+	let tlogo = document.getElementById('t1');
+	let flogo = document.getElementById('l2');
+	let img = "email.png";
+	tlogo.style.color = "black";
+	flogo.src = img;
+}
+
+function log2() {
+	let flogo = document.getElementById('fb');
+	let img = "fb1.png";
+	flogo.src = img;
+}
+function log2out() {
+	let flogo = document.getElementById('fb');
+	let img = "fb.png";
+	flogo.src = img;
+}
+
+function log3() {
+	let flogo = document.getElementById('tw');
+	let img = "twitter1.png";
+	flogo.src = img;
+}
+function log3out() {
+	let flogo = document.getElementById('tw');
+	let img = "twitter.png";
+	flogo.src = img;
+}
+function log4() {
+	let flogo = document.getElementById('yo');
+	let img = "youtube1.png";
+	flogo.src = img;
+}
+function log4out() {
+	let flogo = document.getElementById('yo');
+	let img = "youtube.png";
+	flogo.src = img;
+}
+
+
+function menyover() {
+	let a = document.getElementById('a');	
+	a.style.color = "#5B4477";
+	a.style.borderBottom = "3px solid #5B4477";
+}
+function menyoverout() {
+	let a = document.getElementById('a'[0]);	
+	a.style.color = "gray";
+	a.style.borderBottom = "none";
+}
+
+function menyover1() {
+	let a1 = document.getElementById('a1');		
+	a1.style.color = "#5B4477";
+	a1.style.borderBottom = "3px solid #5B4477";
+}
+function menyover1out() {
+	let a1 = document.getElementById('a1');	
+	a1.style.color = "gray";
+	a1.style.borderBottom = "none";
+}
+
+function menyover2() {
+	let a2 = document.getElementById('a2');		
+	a2.style.color = "#5B4477";
+	a2.style.borderBottom = "3px solid #5B4477";
+}
+function menyover2out() {
+	let a2 = document.getElementById('a2');	
+	a2.style.color = "gray";
+	a2.style.borderBottom = "none";
+}
+
+function menyover3() {
+	let a3 = document.getElementById('a3');		
+	a3.style.color = "#5B4477";
+	a3.style.borderBottom = "3px solid #5B4477";
+}
+function menyover3out() {
+	let a3 = document.getElementById('a3');	
+	a3.style.color = "gray";
+	a3.style.borderBottom = "none";
+}
+
+function menyover4() {
+	let a4 = document.getElementById('a4');		
+	a4.style.color = "#5B4477";
+	a4.style.borderBottom = "3px solid #5B4477";
+}
+function menyover4out() {
+	let a4 = document.getElementById('a4');	
+	a4.style.color = "gray";
+	a4.style.borderBottom = "none";
+}
+
+let backgroundimg = document.getElementById('backgroundimg');
+let backgroundimgs = ["hotel0.jpg" , "hotel1.jgp" , "hotel2.jpg"];
+
+// function imageslide() {
+// 	for (i=0; i=backgroundimgs.length; i++) {
+// 		backgroundimgs.src++;
+// 	};
+// }
