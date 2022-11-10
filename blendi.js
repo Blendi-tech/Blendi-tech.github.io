cc6bd7220a7ddcbfa77d36593453808593a3a44f
Blendi-tech <114534377+Blendi-tech@users.noreply.github.com>
2022-11-10 23:17:22 +0100

    New changes

--- blendi.js
+++ blendi.js
@@ -30,99 +30,87 @@ function log1out() {
 	flogo.src = img;
 }
 
-function log2() {
-	let flogo = document.getElementById('fb');
-	let img = "fb1.png";
-	flogo.src = img;
-}
-function log2out() {
-	let flogo = document.getElementById('fb');
-	let img = "fb.png";
-	flogo.src = img;
-}
+// function log2() {
+// 	let flogo = document.getElementById('fb');
+// 	let img = "fb1.png";
+// 	flogo.src = img;
+// }
+// function log2out() {
+// 	let flogo = document.getElementById('fb');
+// 	let img = "fb.png";
+// 	flogo.src = img;
+// }
 
-function log3() {
-	let flogo = document.getElementById('tw');
-	let img = "twitter1.png";
-	flogo.src = img;
-}
-function log3out() {
-	let flogo = document.getElementById('tw');
-	let img = "twitter.png";
-	flogo.src = img;
-}
-function log4() {
-	let flogo = document.getElementById('yo');
-	let img = "youtube1.png";
-	flogo.src = img;
-}
-function log4out() {
-	let flogo = document.getElementById('yo');
-	let img = "youtube.png";
-	flogo.src = img;
-}
+// function log3() {
+// 	let flogo = document.getElementById('tw');
+// 	let img = "twitter1.png";
+// 	flogo.src = img;
+// }
+// function log3out() {
+// 	let flogo = document.getElementById('tw');
+// 	let img = "twitter.png";
+// 	flogo.src = img;
+// }
+// function log4() {
+// 	let flogo = document.getElementById('yo');
+// 	let img = "youtube1.png";
+// 	flogo.src = img;
+// }
+// function log4out() {
+// 	let flogo = document.getElementById('yo');
+// 	let img = "youtube.png";
+// 	flogo.src = img;
+// }
 
 
-function menyover() {
-	let a = document.getElementById('a');	
-	a.style.color = "#5B4477";
-	a.style.borderBottom = "3px solid #5B4477";
-}
-function menyoverout() {
-	let a = document.getElementById('a'[0]);	
-	a.style.color = "gray";
-	a.style.borderBottom = "none";
-}
-
-function menyover1() {
-	let a1 = document.getElementById('a1');		
-	a1.style.color = "#5B4477";
-	a1.style.borderBottom = "3px solid #5B4477";
-}
-function menyover1out() {
-	let a1 = document.getElementById('a1');	
-	a1.style.color = "gray";
-	a1.style.borderBottom = "none";
-}
-
-function menyover2() {
-	let a2 = document.getElementById('a2');		
-	a2.style.color = "#5B4477";
-	a2.style.borderBottom = "3px solid #5B4477";
-}
-function menyover2out() {
-	let a2 = document.getElementById('a2');	
-	a2.style.color = "gray";
-	a2.style.borderBottom = "none";
-}
-
-function menyover3() {
-	let a3 = document.getElementById('a3');		
-	a3.style.color = "#5B4477";
-	a3.style.borderBottom = "3px solid #5B4477";
-}
-function menyover3out() {
-	let a3 = document.getElementById('a3');	
-	a3.style.color = "gray";
-	a3.style.borderBottom = "none";
-}
-
-function menyover4() {
-	let a4 = document.getElementById('a4');		
-	a4.style.color = "#5B4477";
-	a4.style.borderBottom = "3px solid #5B4477";
-}
-function menyover4out() {
-	let a4 = document.getElementById('a4');	
-	a4.style.color = "gray";
-	a4.style.borderBottom = "none";
-}
-
-let backgroundimg = document.getElementById('backgroundimg');
-let backgroundimgs = ["hotel0.jpg" , "hotel1.jgp" , "hotel2.jpg"];
+// let backgroundimg = document.getElementById('backgroundimg');
+// let backgroundimgs = ["hotel0.jpg" , "hotel1.jgp" , "hotel2.jpg"];
 
 // function imageslide() {
 // 	for (i=0; i=backgroundimgs.length; i++) {
 // 		backgroundimgs.src++;
 // 	};
 // }
+
+let pref = document.getElementById('pref');
+let shtob = document.getElementById('shtof');
+let li = document.getElementById('li');
+
+function text() {
+	li.appendChild(document.createTextNode(pref.value));
+	if (pref.value = 1) {
+		pref.value = " ,";
+	}
+}
+
+shtob.addEventListener("click", text);
+
+
+
+// let fotos = 0;
+// slides();
+
+// function slides() {
+// 	let foto = document.getElementsByClassName("backgroundimg");
+// 	for (i = 0; i < foto.length; i++) {
+// 		foto[i].style.display = "none";
+// 	}
+// 	  fotos++;
+//   if (fotos > foto.length) {fotos = 1}    
+//   foto[fotos-1].style.display = "block";  
+//   setTimeout(slides, 3000);
+// }
+
+let fotos = 0;
+slides();
+
+function slides() {
+	let foto = document.getElementsByClassName("backgroundimg");
+	for (i = 0; i < foto.length; i++) {
+		foto[i].style.display = "none";
+	}
+	fotos++;
+	if (fotos > foto.length) {fotos = 1};
+	foto[fotos-1].style.display = "block";
+	setTimeout(slides, 3000);
+}
